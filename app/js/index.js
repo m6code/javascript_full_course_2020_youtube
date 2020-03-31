3f498581e99a7c4177176ef28ee19c9b946fdf63
M6code <benjamynfrank101@gmail.com>
2020-03-31 09:16:02 +0200

    Working with Variables

--- app/js/index.js
+++ app/js/index.js
@@ -6,3 +6,14 @@ import { log, logTitle } from 'logger';
 /* your imports */
 logTitle('Title')
 /* coding examples */
+
+// Variables
+var name = "Jone Jon";
+var age = 35;
+var hasLicense = true;
+var empty = undefined;
+
+log(name + " type= " + typeof name);
+log(age + " type= " + typeof age);
+log(hasLicense + " type= " +typeof hasLicense);
+log(empty + " type= " + typeof empty);
