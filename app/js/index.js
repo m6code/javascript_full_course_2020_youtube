3d60f086cbd32369434c4dca800416d6fa27c4a7
M6code <benjamynfrank101@gmail.com>
2020-03-31 10:05:38 +0200

    Working with If Statement

--- app/js/index.js
+++ app/js/index.js
@@ -4,38 +4,14 @@ require('styles/main.scss');
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
-logTitle('Loops')
+logTitle('If Statement')
 /* coding examples */
 
-log("For Loops");
-for (var i = 0; i < 3; i++){
-	log(i);
+var bool = false;
+if (bool){
+	log("bool var was true");
+} else if(!bool) {
+	log("bool var was false");
+} else{
+	log("Not found");
 }
-
-
-var persons = [
-	{name: "Alex", age: 23},
-	{name: "Maria", age: 35}
-];
-
-for( var i = 0; i < persons.length; i++){
-	log(persons[i].name);
-	log(persons[i].age);
-	log("-----------------");
-}
-
-log("While Loops");
-var num = 0;
-while(num < 5){
-	log(num);
-	num = num + 1;
-}
-
-
-
-log("Do While Loops");
-var i = 0;
-do{
-	i ++;
-	log(i);
-}while(i < 5)
