edc9843dade6e3dcaec69a7bf8eabb560fae6c65
M6code <benjamynfrank101@gmail.com>
2020-04-01 11:30:47 +0100

    Working with Callbacks

--- app/js/index.js
+++ app/js/index.js
@@ -4,29 +4,13 @@ require('styles/main.scss');
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
-logTitle('Map | Filter | Reduce')
+logTitle('Callbacks')
 /* coding examples */
 
-// Map
-var map = [1,2,3,4,5].map(function(n){
-	return n* 5;
+function callbackEg(name, callback){
+	log(callback(name));
+}
+
+callbackEg("John Drille", function(name){
+	return "Hello " + name;
 });
-
-log("[].map")
-log(map);
-
-// Filter
-log("[].filter");
-var filterEven = [0,1,2,3,4,5,6,7,8,9,10].filter(function(n){
-	return n%2 == 0;
-});
-
-log(filterEven)
-
-//Reduce
-log("[].reduce")
-var reduce = [1,2,3,4,5].reduce(function(accumulator, current){
-	return accumulator + current;
-});
-
-log(reduce);
