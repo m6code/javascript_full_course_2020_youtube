0f4abafad0bfbd99ff7261d9f74d9e22bfa9b7af
M6code <benjamynfrank101@gmail.com>
2020-04-01 11:56:58 +0100

    Working with Import and Exports

---
+++ app/js/Math.js
@@ -0,0 +1,17 @@
+export var add = function(a, b){
+	return a + b;
+}
+
+export var divide = function(a, b){
+	return a / b;
+}
+
+export var multiply = function(a, b){
+	return a * b;
+}
+
+export var subtract = function(a, b){
+	return a - b;
+}
+
+export var PI = 3.1415;
--- app/js/index.js
+++ app/js/index.js
@@ -4,13 +4,21 @@ require('styles/main.scss');
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
-logTitle('Callbacks')
+// import * as Math from './Math'; // Wildcat import
+import {add, divide, PI, multiply, subtract} from './Math'; // Specific import
+logTitle('Named Exports and imports')
 /* coding examples */
 
-function callbackEg(name, callback){
-	log(callback(name));
-}
+// For WildCAT import
+// log(Math.add(2,2));
+// log(Math.divide(20,2));
+// log(Math.multiply(5,2));
+// log(Math.subtract(6,2));
+// log(Math.PI);
 
-callbackEg("John Drille", function(name){
-	return "Hello " + name;
-});
+// For Specific Import
+log(add(33,4));
+log(divide(20,7));
+log(PI);
+log(multiply(3,2));
+log(subtract(4,2));
