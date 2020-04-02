b7d82343e8236b20148a48b501eec0f50f2d3ec4
M6code <benjamynfrank101@gmail.com>
2020-04-02 10:33:00 +0100

    Working withwith Lexical this

--- app/js/index.js
+++ app/js/index.js
@@ -6,39 +6,19 @@ import { log, logTitle } from 'logger';
 /* your imports */
 
 
-logTitle('Arrow Functions')
+logTitle('Lexical this');
 /* coding examples */
+ const person = {
+ 	name: "Alex",
+ 	cars: ["Ferrari","Lambo"],
 
-const hello = () => {
-  const es6 = 'ES6';
-  return `Hello ${es6}`;
-};
-// const hello = function() {
-//   const es6 = 'ES6';
-//   return `Hello ${es6}`;
-// };
+ 	toString: function() {
+ 		// log(`{this.name} has ${this.cars}`);
+ 		this.cars.forEach(car => {
+ 			log(`${this.name} has ${car}`);
+ 		});
+ 	}
+ }
 
+ person.toString();
 
-const powers =
- [1,2,3,4,5].map((number, index) => Math.pow(number, index));
-// const powers = [1,2,3,4,5].map(function(number, index) {
-//   return Math.pow(number, index);
-// });
-
-
-const add = (n1, n2) => n1 + n2;
-// const add = function(n1, n2) {
-//   return n1 + n2;
-// };
-
-
-const milesToKm = miles => miles * 1.60934;
-// const milesToKm = function(miles) {
-//   return miles * 1.60934;
-// };
-
-
-log(hello());
-log(powers);
-log(add(100,100));
-log(milesToKm(200));
