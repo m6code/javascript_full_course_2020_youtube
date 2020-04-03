9e8adc5d0867e6805cebd128cc502b3fc6beff8f
M6code <benjamynfrank101@gmail.com>
2020-04-03 10:37:09 +0100

    Working with Promises

--- app/js/index.js
+++ app/js/index.js
@@ -6,72 +6,22 @@ import { log, logTitle } from 'logger';
 /* your imports */
 
 
-logTitle('Classes inheritance ');
+logTitle('Promises');
 /* coding examples */
 
-/*
--------------------------------------------------
-+ Classes are blueprints
-	* Properties
-	* Behaviours
--------------------------------------------------	
-+ OOP?
+const promise = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve("data back from server");
+	}, 3000);
 
-_________________________________________________
-+ We can model real world examples
-	* Animal, Human, Ball, Shoe, Anything really
--------------------------------------------------
-*/
-
-class Animal{
-	constructor(name, age){
-		log(`${name} is an animal, ${age} years and was created`);
-		this.age = age;
-		this.name = name;
-	}
-
-	eat() {
-		log(`${this.name} is eating`);
-	}
-
-	sleep(){
-		log(`${this.name} is sleeping`);
-	}
-
-	static aStaticMethod(){
-		log(`I am a static method you don't need an instance the Animal class to call me`)
-	}
-}
+	setTimeout(() => {
+		reject(" Error, no data back from server yet");
+	}, 5000);
+});
 
 
-
-const sparkles = new Animal("Sparkles", 4);
-sparkles.eat();
-sparkles.sleep();
-
-log("------------------------------------------------");
-const masta = new Animal("Beast", 44);
-masta.eat();
-masta.sleep();
-
-log("------------------------------------------------");
-
-class Dog  extends Animal{
-	constructor(name, age, breed){
-		super(name, age);
-		this.breed = breed;
-	}
-
-	showBreed(){
-		log(`${this.breed} is ${this.age} years and of ${this.breed} breed`);
-	}
-}
-
-
-const mk = new Dog("MiliKa", 2 ,"BullDog");
-mk.showBreed();
-mk.sleep();
-mk.eat();
-
-log("_________________________________________________")
-Animal.aStaticMethod();
+promise.then(response =>{
+	log(response);
+}).catch(error => {
+	log(error);
+});
