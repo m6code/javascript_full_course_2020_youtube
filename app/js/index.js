28c62072ba4907631e8b99884c56c87a4c0853dc
M6code <benjamynfrank101@gmail.com>
2020-04-03 11:56:36 +0100

    Working with Async Await

--- app/js/index.js
+++ app/js/index.js
@@ -5,37 +5,57 @@ import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
 
-import {coroutine as co} from 'bluebird';
 
-
-logTitle('Generators');
+logTitle('Async Await');
 /* coding examples */
 
-// const getRandUsers = n => {
-// 	const fetchRandUsers = fetch(`https://randomuser.me/api/?results=${n}`);
-// 	fetchRandUsers.then(data => {
-// 		data.json().then(randUsers => {
-// 			log(JSON.stringify(randUsers.results.length));
-// 			randUsers.results.forEach(user => {
-// 				const {gender, name :{title: ti, first :  fname, last: lName} ,email} = user;
-// 				log(`${gender} - ${ti}, ${fname} ${lName} - ${email}`)
-// 			})
-// 		})
-// 	});
-// }
 
-// getRandUsers(50);
+async function logName(name){
+	log(name);
+	// 1. we can yield promises using await
+	const transformName = new Promise((resolve, reject) => {
+		setTimeout(() => resolve(name.toUpperCase()), 1000);
+	});
 
-const getRandUsers = co(function* (n){
-	const fetchRandUsers = yield fetch(`https://randomuser.me/api/?results=${n}`);
-	const data = yield fetchRandUsers.json();
-	return data;
+	const result = await transformName;
+	log(result);
+	// 2. return a promise
+	return name;	
+}
+
+logName("Hafausatu").then(result => {
+	log("result from async function = " + result);
 });
 
-getRandUsers(50).then(randUsers => {
-	log(JSON.stringify(randUsers.results.length));
-	randUsers.results.forEach(user => {
+
+
+
+// const getRandUsers = co(function* (n){
+// 	const fetchRandUsers = yield fetch(`https://randomuser.me/api/?results=${n}`);
+// 	const data = yield fetchRandUsers.json();
+// 	return data;
+// });
+
+// getRandUsers(50).then(randUsers => {
+// 	log(JSON.stringify(randUsers.results.length));
+// 	randUsers.results.forEach(user => {
+// 		const {gender, name :{title: ti, first :  fname, last: lName} ,email} = user;
+// 		log(`${gender} - ${ti}, ${fname} ${lName} - ${email}`)
+// 	})
+// }).catch(err => log);
+
+
+const getRandUsers = async n => {
+	const fetchRandUsers = await fetch(`https://randomuser.me/api/?results=${n}`);
+	const data = await fetchRandUsers.json();
+
+	data.results.forEach(user => {
 		const {gender, name :{title: ti, first :  fname, last: lName} ,email} = user;
-		log(`${gender} - ${ti}, ${fname} ${lName} - ${email}`)
-	})
-}).catch(err => log);
+		log(`${gender} - ${ti}, ${fname} ${lName} - ${email}`);
+	});
+
+	return data;
+};
+
+
+getRandUsers(100);
